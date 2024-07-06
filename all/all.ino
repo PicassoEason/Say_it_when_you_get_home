a17b795fbd348307359d6fc7885d87a1f5d4aa1e
picassoeason <easonwu.student@outlook.com>
2024-07-06 16:00:36 +0800

    優化code

--- all/all.ino
+++ all/all.ino
@@ -2,93 +2,59 @@
 #include <WebSocketsServer.h>
 #include <SPIFFS.h>
 #include <driver/i2s.h>
-#include <Arduino.h>
 #include <WiFi.h>
 #include <Audio.h>
 
 #define BUTTON_PIN 34
-#define VIBRATION_PIN 33
 #define SERVO_PIN 14
-
-// INMP441 設置
 #define I2S_WS 25
 #define I2S_SD 33
 #define I2S_SCK 32
 #define I2S_PORT I2S_NUM_0
 #define I2S_SAMPLE_RATE 16000
-#define I2S_SAMPLE_BITS 16
 #define I2S_READ_LEN (16 * 1024)
 #define RECORD_TIME 5
-
 #define I2S_DOUT 25
 #define I2S_BCLK 27
 #define I2S_LRC 26
 
 Servo myservo;
-WebSocketsServer webSocket = WebSocketsServer(81);
+WebSocketsServer webSocket(81);
 Audio audio;
 
-// WiFi設置
 const char* ssid = "Era";
 const char* password = "123321123";
+const char filename[] PROGMEM = "/audio.wav";
 
-// 全域變數
 int buttonState = HIGH;
 int lastButtonState = HIGH;
 bool servoState = false;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
 
-// 音頻相關
-const char filename[] = "/audio.wav";
-const size_t bufferSize = 4096;
-
-struct WAVHeader {
-    char riff[4] = {'R', 'I', 'F', 'F'};
-    uint32_t chunkSize;
-    char wave[4] = {'W', 'A', 'V', 'E'};
-    char fmt[4] = {'f', 'm', 't', ' '};
-    uint32_t subchunk1Size = 16;
-    uint16_t audioFormat = 1;
-    uint16_t numChannels = 1;
-    uint32_t sampleRate = I2S_SAMPLE_RATE;
-    uint32_t byteRate = I2S_SAMPLE_RATE * 2;
-    uint16_t blockAlign = 2;
-    uint16_t bitsPerSample = 16;
-    char data[4] = {'d', 'a', 't', 'a'};
-    uint32_t subchunk2Size;
-};
-
 void setup() {
     Serial.begin(115200);
-    Serial.println("System initialized");
-
     myservo.attach(SERVO_PIN);
     pinMode(BUTTON_PIN, INPUT);
-    pinMode(VIBRATION_PIN, INPUT);
-
     if (!SPIFFS.begin(true)) {
-        Serial.println("SPIFFS初始化失敗");
+        Serial.println(F("SPIFFS初始化失敗"));
         return;
     }
-
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
-        Serial.println("正在連接WiFi...");
+        Serial.println(F("正在連接WiFi..."));
     }
-    Serial.println("WiFi已連接");
-    Serial.print("IP地址: ");
+    Serial.println(F("WiFi已連接"));
+    Serial.print(F("IP地址: "));
     Serial.println(WiFi.localIP());
-
     websocket_setup();
-    voicer_setup();
+    audio_setup();
 }
 
 void websocket_setup() {
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
-
     i2s_config_t i2s_config = {
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
         .sample_rate = I2S_SAMPLE_RATE,
@@ -103,7 +69,6 @@ void websocket_setup() {
         .fixed_mclk = 0
     };
     i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
-
     i2s_pin_config_t pin_config = {
         .bck_io_num = I2S_SCK,
         .ws_io_num = I2S_WS,
@@ -113,7 +78,7 @@ void websocket_setup() {
     i2s_set_pin(I2S_PORT, &pin_config);
 }
 
-void voicer_setup() {
+void audio_setup() {
     audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
     audio.setVolume(10000);
     audio.connecttohost("https://firebasestorage.googleapis.com/v0/b/home9-58f42.appspot.com/o/ddd16000hz16bp.wav?alt=media&token=c70a5283-9bca-4550-a141-80eb57e39c28");
@@ -128,26 +93,18 @@ void loop() {
 
 void btn_loop() {
     int reading = digitalRead(BUTTON_PIN);
-    
     if (reading != lastButtonState) {
         lastDebounceTime = millis();
     }
-
     if ((millis() - lastDebounceTime) > debounceDelay) {
         if (reading != buttonState) {
             buttonState = reading;
-            
             if (buttonState == LOW) {
-                Serial.println("press");
                 servoState = !servoState;
                 myservo.write(servoState ? 180 : 0);
-                Serial.println(servoState ? "舵機轉到180度" : "舵機轉到0度");
-            } else {
-                Serial.println("x");
             }
         }
     }
-
     lastButtonState = reading;
 }
 
@@ -163,19 +120,19 @@ void voice_loop() {
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
     switch(type) {
         case WStype_DISCONNECTED:
-            Serial.printf("[%u] 斷開連接!\n", num);
+            Serial.printf_P(PSTR("[%u] 斷開連接!\n"), num);
             break;
         case WStype_CONNECTED:
             {
                 IPAddress ip = webSocket.remoteIP(num);
-                Serial.printf("[%u] 連接來自 %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+                Serial.printf_P(PSTR("[%u] 連接來自 %d.%d.%d.%d\n"), num, ip[0], ip[1], ip[2], ip[3]);
             }
             break;
         case WStype_TEXT:
-            Serial.printf("收到文本消息: %s\n", payload);
+            Serial.printf_P(PSTR("收到文本消息: %s\n"), payload);
             break;
         case WStype_BIN:
-            Serial.println("收到二進制消息");
+            Serial.println(F("收到二進制消息"));
             break;
         default:
             break;
@@ -183,15 +140,28 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
 }
 
 void recordAudioWAV() {
-    Serial.println("開始錄音...");
     File file = SPIFFS.open(filename, FILE_WRITE);
     if(!file){
-        Serial.println("無法打開文件進行寫入");
+        Serial.println(F("無法打開文件進行寫入"));
         return;
     }
-
-    WAVHeader header;
-    size_t headerSize = sizeof(WAVHeader);
+    struct {
+        char riff[4] = {'R', 'I', 'F', 'F'};
+        uint32_t chunkSize;
+        char wave[4] = {'W', 'A', 'V', 'E'};
+        char fmt[4] = {'f', 'm', 't', ' '};
+        uint32_t subchunk1Size = 16;
+        uint16_t audioFormat = 1;
+        uint16_t numChannels = 1;
+        uint32_t sampleRate = I2S_SAMPLE_RATE;
+        uint32_t byteRate = I2S_SAMPLE_RATE * 2;
+        uint16_t blockAlign = 2;
+        uint16_t bitsPerSample = 16;
+        char data[4] = {'d', 'a', 't', 'a'};
+        uint32_t subchunk2Size;
+    } header;
+    
+    size_t headerSize = sizeof(header);
     size_t wavSize = headerSize + RECORD_TIME * I2S_SAMPLE_RATE * 2;
     header.chunkSize = wavSize - 8;
     header.subchunk2Size = wavSize - headerSize;
@@ -206,65 +176,21 @@ void recordAudioWAV() {
         i2s_read(I2S_PORT, (void*) i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY);
         file.write(i2s_read_buff, i2s_read_len);
     }
-
     file.close();
-    Serial.println("錄音完成");
-    Serial.printf("已寫入 %u 字節\n", wavSize);
 }
 
 void sendAudioWAV() {
-    Serial.println("開始發送音頻...");
     File file = SPIFFS.open(filename, FILE_READ);
     if(!file){
-        Serial.println("無法打開文件進行讀取");
+        Serial.println(F("無法打開文件進行讀取"));
         return;
     }
-
+    const size_t bufferSize = 1024;
     uint8_t buffer[bufferSize];
     size_t bytesRead;
-
     while((bytesRead = file.read(buffer, bufferSize)) > 0){
         webSocket.broadcastBIN(buffer, bytesRead);
         delay(5);
     }
-
     file.close();
-    Serial.println("音頻發送完成");
-}
-
-// Audio callback functions
-void audio_info(const char *info){
-    Serial.print("info        "); Serial.println(info);
-}
-
-void audio_id3data(const char *info){
-    Serial.print("id3data     ");Serial.println(info);
-}
-
-void audio_eof_mp3(const char *info){
-    Serial.print("eof_mp3     ");Serial.println(info);
-}
-
-void audio_showstation(const char *info){
-    Serial.print("station     ");Serial.println(info);
-}
-
-void audio_showstreamtitle(const char *info){
-    Serial.print("streamtitle ");Serial.println(info);
-}
-
-void audio_bitrate(const char *info){
-    Serial.print("bitrate     ");Serial.println(info);
-}
-
-void audio_commercial(const char *info){
-    Serial.print("commercial  ");Serial.println(info);
-}
-
-void audio_icyurl(const char *info){
-    Serial.print("icyurl      ");Serial.println(info);
-}
-
-void audio_lasthost(const char *info){
-    Serial.print("lasthost    ");Serial.println(info);
 }
